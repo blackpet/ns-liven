16c7f9094a5e1e280514b5b55f413ad6d473b5db
JaeHo Sim <blackpet@gmail.com>
2020-07-02 16:11:53 +0900

    [Live.N --> nsedu] 통신 구조를 [client --> polka server --> nsedu] Proxy 구조로 변경!!

--- src/env.js
+++ src/env.js
@@ -6,6 +6,6 @@ const upload = 'http://172.26.10.103:8080'
 export const env = {
   // api: 'http://localhost:4004'
   nsedu,
-  api: nsedu + '/liven',
+  api: '/api/liven',
   resource: upload
 }
--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import http from 'http';
 import sirv from 'sirv';
 import polka from 'polka';
+import {createProxyMiddleware} from 'http-proxy-middleware'
 import compression from 'compression';
 import * as sapper from '@sapper/server';
 import LivenServer from "./server/liven-server";
+import {env} from './env'
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
@@ -11,6 +13,14 @@ const dev = NODE_ENV === 'development';
 const server = http.createServer();
 
 polka({server})
+	.use('/api', createProxyMiddleware({
+		changeOrigin: true,
+		logLevel: 'debug',
+		target: env.nsedu,
+		pathRewrite: {
+			'^/api': ''
+		}
+	}))
 	.use(
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
